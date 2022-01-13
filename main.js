39f9f11e50aa4033cd5edae70b518cef5c59a1a9
Ender Malcolm <89848972+Ender-Malcolm@users.noreply.github.com>
2022-01-13 13:36:50 +0000

    local storage additions, firebase integration, changed two tasks to true

--- main.js
+++ main.js
@@ -3,3 +3,46 @@ import './style.css'
 
 window.Alpine = Alpine
 Alpine.start()
+
+// Add event listners for connection errors
+
+if (navigator.onLine) {
+  document.querySelector('#netWarning').style.display = 'none'
+}
+else {
+  document.querySelector('#netWarning').style.display = ''
+}
+
+window.addEventListener('offline', function(e) {
+  console.log('offline');
+  document.querySelector('#netWarning').style.display = ''
+});
+
+window.addEventListener('online', function(e) {
+  console.log('online');
+  document.querySelector('#netWarning').style.display = 'none'
+});
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env['apiKey'],
+  authDomain: "todoapp-56c5a.firebaseapp.com",
+  projectId: "todoapp-56c5a",
+  storageBucket: "todoapp-56c5a.appspot.com",
+  messagingSenderId: "932926933534",
+  appId: "1:932926933534:web:5f32fd57de43d7d749bc0f",
+  measurementId: "G-25YW4FBH96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
